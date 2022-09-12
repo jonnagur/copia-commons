376a21411a834de3271eaeecded7096cbc3be278
juanluispaulanotorres <juanluis.paulano@bosonit.com>
2022-09-12 17:38:41 +0200

    instalación de dependencias de bootstrap/pendiente de visualización de spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HayaFenixLibraryModule } from 'projects/haya-fenix-library/src/lib/haya-fenix-library.module';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
